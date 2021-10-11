206b9cc390e46d02789daa0c4c06102c1959743d
alphanoobie <hussaingadi.hg@gmail.com>
2021-10-11 17:19:14 +0530

    add db and display one random word on website

--- GRE_words_webapp.py
+++ GRE_words_webapp.py
@@ -1,13 +1,22 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.automap import automap_base
+from numpy import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///words.db'
 db = SQLAlchemy(app)
 
+#word_db = db.Table('Words', db.metadata, autoload=True, autoload_with=db.engine)
+Base = automap_base()
+Base.prepare(db.engine, reflect = True)
+Words = Base.classes.Words
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    x = random.randint(1,333)
+    random_row = db.session.query(Words).get(x)
+    return render_template("index.html", random_row=random_row)
  
 if __name__ == "__main__":
     app.run()
